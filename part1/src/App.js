d6d42b808ef66ed5e67c1ec2b1903f84bcba8d39
Toni Hukka <berendin@gmail.com>
2021-03-17 18:57:38 +0200

    part1 example app

---
+++ part1/src/App.js
@@ -0,0 +1,22 @@
+import React from 'react'
+
+const Hello = () => {
+  return (
+    <div>
+      <p>Hello world</p>
+    </div>
+  )
+}
+
+const App = () => {
+  return (
+    <div>
+      <h1>Greetings</h1>
+      <Hello />
+      <Hello />
+      <Hello />
+    </div>
+  )
+}
+
+export default App;
