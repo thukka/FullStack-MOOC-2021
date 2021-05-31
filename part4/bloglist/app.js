fc47f70eb98fca731ded0184b09a5d998a59966b
Toni Hukka <berendin@gmail.com>
2021-05-31 10:32:32 +0300

    4.2 done, refactored code into controllers, models, utils. installed eslint, dotenv and morgan

---
+++ part4/bloglist/app.js
@@ -0,0 +1,28 @@
+const config = require('./utils/config');
+const express = require('express');
+const app = express();
+const cors = require('cors');
+const logger = require('./utils/logger');
+const blogRouter = require('./controllers/blog');
+const middleware = require('./utils/middleware');
+const mongoose = require('mongoose');
+
+const mongoUrl = config.MONGODB_URI;
+mongoose.connect(mongoUrl, { useNewUrlParser: true, useUnifiedTopology: true, useFindAndModify: false, useCreateIndex: true })
+    .then(
+        logger.info('Connected to MongoDB')
+    ).catch(err => {
+        logger.error('Error connecting to MongoDB', err);
+    }
+    );
+
+app.use(cors());
+app.use(express.json());
+app.use(middleware.morgan('tiny'));
+
+app.use('/api/blogs/', blogRouter);
+
+app.use(middleware.unknownEndpoint);
+app.use(middleware.errorHandler);
+
+module.exports = app;
