92be53cc02b0a2085007ee79003297486ad7f628
Toni Hukka <berendin@gmail.com>
2021-07-10 12:55:16 +0300

    testing supertest

--- part4/bloglist/utils/config.js
+++ part4/bloglist/utils/config.js
@@ -1,5 +1,7 @@
 require('dotenv').config();
 let PORT = process.env.PORT;
-let MONGODB_URI = process.env.MONGODB_URI;
+let MONGODB_URI = process.env.NODE_ENV === 'test'
+    ? process.env.TEST_MONGODB_URI
+    : process.env.MONGODB_URI;
 
 module.exports = { PORT, MONGODB_URI };
