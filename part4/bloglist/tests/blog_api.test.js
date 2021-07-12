89608953a80ba489aa0b461e01a9d0d0948013d9
Toni Hukka <berendin@gmail.com>
2021-07-12 17:10:36 +0300

    (4.10) test for creating a new blog

--- part4/bloglist/tests/blog_api.test.js
+++ part4/bloglist/tests/blog_api.test.js
@@ -21,9 +21,26 @@ test('correct amount of blogs is returned', async () => {
     expect(response.body).toHaveLength(helper.initialBlogs.length);
 });
 
-test('returned blog identifier should be defined as "id"', async() => {
+test('returned blog identifier should be defined as "id"', async () => {
     const response = await api.get('/api/blogs/');
     expect(response.body[0].id).toBeDefined();
+});
+
+test('adding a new blog succesfully', async () => {
+    const newBlog =
+        { 'author': 'Supertest Blog', 'likes': 777, 'title': 'Supertest Blogzone', 'url': 'http://www.testtest.test' };
+
+    await api
+        .post('/api/blogs')
+        .send(newBlog)
+        .expect(201)
+        .expect('Content-Type', /application\/json/);
+    
+    const blogsAtEnd = await helper.blogsInDb();
+    expect(blogsAtEnd).toHaveLength(helper.initialBlogs.length + 1);
+
+    const contents = blogsAtEnd.map(b => b.author);
+    expect(contents).toContain('Supertest Blog');
 });
 
 afterAll(() => {
--- part4/bloglist/tests/testHelper.js
+++ part4/bloglist/tests/testHelper.js
@@ -1,9 +1,9 @@
 const Blog = require('../models/blogreview');
 
 const initialBlogs = [
-    { 'author': 'Teppo Testaaja', 'id': '60e967ee6daeb1134b58114c', 'likes': 321, 'title': 'Testaajan ruokanurkka', 'url': 'http://www.testzone.fi' },
-    { 'author': 'Teppo Testaaja', 'id': '60e968026daeb1134b58114d', 'likes': 555, 'title': 'Sipulikeitto', 'url': 'http://www.sipulikeitto.fi' },
-    { 'author': 'Kalle Kalamies', 'id': '60ec2ca2fbbe6b0d4448da41', 'likes': 5, 'title': '3rd edition of how to cook with fire', 'url': 'http://www.kokkikirja.fi' }
+    { 'author': 'Teppo Testaaja', 'likes': 321, 'title': 'Testaajan ruokanurkka', 'url': 'http://www.testzone.fi' },
+    { 'author': 'Teppo Testaaja', 'likes': 555, 'title': 'Sipulikeitto', 'url': 'http://www.sipulikeitto.fi' },
+    { 'author': 'Kalle Kalamies', 'likes': 5, 'title': '3rd edition of how to cook with fire', 'url': 'http://www.kokkikirja.fi' }
 ];
 
 const blogsInDb = async () => {
