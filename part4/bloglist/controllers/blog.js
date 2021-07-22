a7d93069d9d722f65a0e1a44c8090d1ff517f6f0
Toni Hukka <berendin@gmail.com>
2021-07-22 11:02:53 +0300

    (4.17) blogs can be added with a user, get api now populates users and blogs

--- part4/bloglist/controllers/blog.js
+++ part4/bloglist/controllers/blog.js
@@ -1,8 +1,9 @@
 const blogRouter = require('express').Router();
 const Blog = require('../models/blogreview');
+const User = require('../models/user');
 
 blogRouter.get('/', async (request, response) => {
-    const blogList = await Blog.find({});
+    const blogList = await Blog.find({}).populate('user', { username: 1, name: 1, id: 1 });
     response.json(blogList.map(blog => blog.toJSON()));
 });
 
@@ -12,12 +13,21 @@ blogRouter.get('/:id', async (request, response) => {
 });
 
 blogRouter.post('/', async (request, response) => {
-    const blog = new Blog(request.body);
-    if (blog.likes === undefined) {
-        blog.likes = 0;
-    }
+    const body = request.body;
+    const user = await User.findById(body.userId);
+
+    const blog = new Blog({
+        'title': body.title,
+        'likes': body.likes === undefined ? body.likes = 0 : body.likes,
+        'author': body.author,
+        'url': body.url,
+        'user': user._id
+    });
 
     const savedBlog = await blog.save();
+    user.blogs = user.blogs.concat(savedBlog._id);
+    await user.save();
+
     response.json(savedBlog.toJSON());
 });
 
--- part4/bloglist/models/blogreview.js
+++ part4/bloglist/models/blogreview.js
@@ -13,6 +13,10 @@ const blogSchema = mongoose.Schema({
     likes: {
         type: Number,
         required: true
+    },
+    user: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'User'
     }
 });
 
