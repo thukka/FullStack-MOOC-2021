a61115bb70b56bf3e876b34e7733e9eb59b07d09
Toni Hukka <berendin@gmail.com>
2021-08-02 14:03:24 +0300

    (5.6 and 5.7) additional info on blogs can be viewed

--- part5/bloglist-frontend/src/components/Blog.js
+++ part5/bloglist-frontend/src/components/Blog.js
@@ -1,8 +1,47 @@
-import React from 'react'
-const Blog = ({blog}) => (
-  <div>
-    {blog.title} {blog.author}
-  </div>  
-)
+import React, { useState } from 'react'
+
+const Blog = ({ blog }) => {
+  const [additionalInfo, setAdditionalInfo] = useState(false)
+
+  const toggleAdditionalInfo = () => setAdditionalInfo(!additionalInfo)
+
+  const blogStyle = {
+    paddingTop: 10,
+    paddingLeft: 2,
+    border: 'solid',
+    borderWidth: 1,
+    marginBottom: 5
+  }
+
+  const BasicInfoLayout = () => (
+    <div style={blogStyle}>
+      {blog.title} {blog.author} <button onClick={toggleAdditionalInfo}>view</button>
+    </div>
+  )
+
+  const AddInfoLayout = () => (
+    <div style={blogStyle}>
+      <p>{blog.title}
+      <button onClick={toggleAdditionalInfo}>hide</button>
+      </p>
+      <p>{blog.url}</p>
+      <p>likes {blog.likes}
+      <button onClick={() => console.log('like clicked')}>like</button>
+      </p>
+      <p>{blog.author}</p>
+    </div>
+  )
+
+  if (additionalInfo === true) {
+    return (
+      AddInfoLayout()
+    )
+  }
+
+  return (
+    BasicInfoLayout()
+  )
+
+}
 
 export default Blog
