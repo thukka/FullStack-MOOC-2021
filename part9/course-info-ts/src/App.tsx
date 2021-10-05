544d1eb5c995a1d014c021ae2a69a2777c172154
Toni Hukka <berendin@gmail.com>
2021-10-05 10:53:11 +0300

    (9.15) union types and exhaustive type checking

--- part9/course-info-ts/src/App.tsx
+++ part9/course-info-ts/src/App.tsx
@@ -1,25 +1,72 @@
 import React from 'react';
 
-interface CourseParts {
-  name: string,
-  exerciseCount: number
+interface CoursePartBase {
+  name: string;
+  exerciseCount: number;
+  type: string;
 }
+
+interface CourseWithDescription extends CoursePartBase {
+  description: string;
+}
+
+interface CourseNormalPart extends CourseWithDescription {
+  type: 'normal';
+}
+
+interface CourseProjectPart extends CoursePartBase {
+  type: 'groupProject';
+  groupProjectCount: number;
+}
+
+interface CourseSubmissionpart extends CourseWithDescription {
+  type: 'submission';
+  exerciseSubmissionLink: string;
+}
+
+interface CourseExtendedInfo extends CourseWithDescription {
+  type: 'special';
+  requirements: Array<string>;
+}
+
+type CoursePart = CourseNormalPart | CourseProjectPart | CourseSubmissionpart | CourseExtendedInfo;
+
+const assertNever = (value: never): never => {
+  throw new Error(`
+  Unhandled discriminated union member: ${JSON.stringify(value)}
+  `)
+};
+
+const Part = ({ part }: { part: CoursePart }) => {
+  switch (part.type) {
+    case 'normal':
+      return <p><strong>Name: {part.name}</strong> <br /> Exercise count: {part.exerciseCount} <br /> Description: {part.description} <br /> Type: {part.type}</p>
+    case 'groupProject':
+      return <p><strong>Name: {part.name}</strong> <br /> Exercise count: {part.exerciseCount} <br /> Group Project Count: {part.groupProjectCount} <br /> Type: {part.type}</p>
+    case 'submission':
+      return <p><strong>Name: {part.name}</strong> <br /> Exercise count: {part.exerciseCount} <br /> Description: {part.description} <br /> Exercise Submission Link: {part.exerciseSubmissionLink} <br /> Type: {part.type}</p>
+    case 'special':
+      return <p><strong>Name: {part.name}</strong> <br /> Exercise count: {part.exerciseCount} <br /> Description: {part.description} <br /> Requirements: {part.requirements.join(', ')} <br /> Type: {part.type}</p>
+    default:
+      return assertNever(part);
+  }
+};
 
 const Header = ({ name }: { name: string }) => {
   return <h1>{name}</h1>
 };
 
-const Content = ({ parts }: { parts: Array<CourseParts> }) => {
+const Content = ({ parts }: { parts: Array<CoursePart> }) => {
   return (
-    <>
+    <div>
       {parts.map(p => {
-        return <p key={p.name}> {p.name} {p.exerciseCount}</p>
+        return <Part part={p} key={p.name} />
       })}
-    </>
+    </div>
   );
 };
 
-const Total = ({ parts }: { parts: Array<CourseParts> }) => {
+const Total = ({ parts }: { parts: Array<CoursePart> }) => {
   return (
     <p>
       Number of exercises{" "}
@@ -30,20 +77,40 @@ const Total = ({ parts }: { parts: Array<CourseParts> }) => {
 
 const App = () => {
   const courseName = 'Half Stack application Development';
-  const courseParts = [
+  const courseParts: CoursePart[] = [
     {
-      name: 'Fundamentals',
-      exerciseCount: 10
+      name: "Fundamentals",
+      exerciseCount: 10,
+      description: "This is the leisured course part",
+      type: "normal"
+    },
+    {
+      name: "Advanced",
+      exerciseCount: 7,
+      description: "This is the harded course part",
+      type: "normal"
     },
     {
       name: "Using props to pass data",
-      exerciseCount: 7
+      exerciseCount: 7,
+      groupProjectCount: 3,
+      type: "groupProject"
     },
     {
       name: "Deeper type usage",
-      exerciseCount: 14
+      exerciseCount: 14,
+      description: "Confusing description",
+      exerciseSubmissionLink: "https://fake-exercise-submit.made-up-url.dev",
+      type: "submission"
+    },
+    {
+      name: "Backend development",
+      exerciseCount: 21,
+      description: "Typing the backend",
+      requirements: ["nodejs", "jest"],
+      type: "special"
     }
-  ];
+  ]
 
   return (
     <div>
