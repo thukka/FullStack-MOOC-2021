da92023a6b2b8cdf3f7ebad17e46a3b31991ca4e
Toni Hukka <berendin@gmail.com>
2021-10-11 10:16:43 +0300

    (9.19) new types for patient data

--- part9/patientor/backend/src/services/patientService.ts
+++ part9/patientor/backend/src/services/patientService.ts
@@ -1,4 +1,4 @@
-import patientData from '../../data/patients.json';
+import patientData from '../../data/patients';
 import { Patient, SensitivePatientData, NewPatient } from '../types';
 import { v1 as uuid } from 'uuid';
 
--- part9/patientor/backend/src/types.ts
+++ part9/patientor/backend/src/types.ts
@@ -1,13 +1,50 @@
-export interface DiagnoseInterface {
+export interface Diagnosis {
     code: string;
     name: string;
     latin?: string;
 }
 
-// eslint-disable-next-line @typescript-eslint/no-empty-interface
-export interface Entry {
-
+interface BaseEntry {
+    id: string;
+    description: string;
+    date: string;
+    specialist: string;
+    diagnosisCodes?: Array<Diagnosis['code']>;
 }
+
+export enum HealthCheckRating {
+    'Healthy' = 0,
+    'LowRisk' = 1,
+    'HighRisk' = 2,
+    'CriticalRisk' = 3
+}
+
+interface HealthCheckEntry extends BaseEntry {
+    type: 'HealthCheck';
+    healthCheckRating: HealthCheckRating;
+}
+
+interface OccupationalHealthcareEntry extends BaseEntry {
+    type: 'OccupationalHealthcare';
+    employerName: string;
+    sickLeave?: { 
+        startDate: string;
+        endDate: string;
+    }
+}
+
+interface HospitalEntry extends BaseEntry {
+    type: 'Hospital';
+    discharge: {
+        date: string;
+        criteria: string;
+    }
+}
+
+export type Entry =
+    | HospitalEntry
+    | OccupationalHealthcareEntry
+    | HealthCheckEntry;
 
 export interface Patient {
     id: string;
