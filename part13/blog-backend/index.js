d8ac4c3f4b87eac59d6ed2e67af63976c8b5c781
Toni Hukka <berendin@gmail.com>
2022-04-13 11:34:56 +0300

    (13.5) refactor to make code more logical

--- part13/blog-backend/index.js
+++ part13/blog-backend/index.js
@@ -1,79 +1,19 @@
-require('dotenv').config()
-const { Sequelize, Model, DataTypes } = require('sequelize')
 const express = require('express')
 const app = express()
 
-const sequelize = new Sequelize(process.env.DATABASE_URL, {
-    dialectOptions: {
-        ssl: {
-            require: true,
-            rejectUnauthorized: false
-        }
-    }
-})
+const { PORT } = require('./util/config')
+const { connectToDatabase } = require('./util/db')
 
-class Blog extends Model { }
-Blog.init({
-    id: {
-        type: DataTypes.INTEGER,
-        primaryKey: true,
-        autoIncrement: true
-    },
-    author: {
-        type: DataTypes.TEXT
-    },
-    url: {
-        type: DataTypes.TEXT,
-        allowNull: false
-    },
-    title: {
-        type: DataTypes.TEXT,
-        allowNull: false
-    },
-    likes: {
-        type: DataTypes.INTEGER,
-        defaultValue: 0,
-        allowNull: false
-    }
-}, {
-    sequelize,
-    underscored: true,
-    timestamps: true,
-    modelName: 'blog'
-})
-Blog.sync()
+const blogsRouter = require('./controllers/blogs')
 
 app.use(express.json())
+app.use('/api/blogs', blogsRouter)
 
-app.get('/api/blogs', async (req, res) => {
-    const blogs = await Blog.findAll()
-    res.json(blogs)
-})
+const start = async () => {
+    await connectToDatabase()
+    app.listen(PORT, () => {
+        console.log(`Server running on port ${PORT}`)
+    })
+}
 
-app.post('/api/blogs', async (req, res) => {
-    try {
-        const blog = await Blog.create(req.body)
-        return res.json(blog)
-    } catch (err) {
-        return res.status(404).json({ err })
-    }
-})
-
-app.delete('/api/blogs/:id', async (req, res) => {
-    const blog = await Blog.findByPk(req.params.id)
-    if (blog) {
-        try {
-            await blog.destroy()
-            return res.status(200).end()
-        } catch (err) {
-            return res.status(404).json({ err })
-        }
-    } else {
-        res.status(404).end()
-    }
-})
-
-const PORT = process.env.PORT || 3001
-app.listen(PORT, () => {
-    console.log(`Server running on port ${PORT}`)
-})
+start()
