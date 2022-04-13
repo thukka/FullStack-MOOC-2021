4c169ffef07833e2275be3b5f27be867238a72de
Toni Hukka <berendin@gmail.com>
2022-04-13 14:34:00 +0300

    created middleware for finding requested blog

--- part13/blog-backend/controllers/blogs.js
+++ part13/blog-backend/controllers/blogs.js
@@ -1,6 +1,11 @@
 const router = require('express').Router()
 const { Blog } = require('../models/')
 
+const findBlog = async (req, res, next) => {
+    req.blog = await Blog.findByPk(req.params.id)
+    next()
+}
+
 router.get('/', async (req, res) => {
     const blogs = await Blog.findAll()
     res.json(blogs)
@@ -15,11 +20,10 @@ router.post('/', async (req, res) => {
     }
 })
 
-router.delete('/:id', async (req, res) => {
-    const blog = await Blog.findByPk(req.params.id)
-    if (blog) {
+router.delete('/:id', findBlog, async (req, res) => {
+    if (req.blog) {
         try {
-            await blog.destroy()
+            await req.blog.destroy()
             return res.status(200).end()
         } catch (err) {
             return res.status(404).json({ err })
@@ -29,11 +33,10 @@ router.delete('/:id', async (req, res) => {
     }
 })
 
-router.put('/:id', async (req, res) => {
-    const blog = await Blog.findByPk(req.params.id)
-    if (blog) {
-        blog.likes = req.body.likes
-        await blog.save()
+router.put('/:id', findBlog, async (req, res) => {
+    if (req.blog) {
+        req.blog.likes = req.body.likes
+        await req.blog.save()
         return res.status(200).end()
     } else {
         res.status(404).end()
