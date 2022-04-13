10914df383665ee8861177348ca86f73bc37104b
Toni Hukka <berendin@gmail.com>
2022-04-13 14:58:16 +0300

    (13.7) created error handler middleware

--- part13/blog-backend/controllers/blogs.js
+++ part13/blog-backend/controllers/blogs.js
@@ -11,36 +11,49 @@ router.get('/', async (req, res) => {
     res.json(blogs)
 })
 
-router.post('/', async (req, res) => {
+router.post('/', async (req, res, next) => {
     try {
         const blog = await Blog.create(req.body)
         return res.json(blog)
     } catch (err) {
-        return res.status(404).json({ err })
+        next(err)
     }
 })
 
-router.delete('/:id', findBlog, async (req, res) => {
-    if (req.blog) {
-        try {
-            await req.blog.destroy()
-            return res.status(200).end()
-        } catch (err) {
-            return res.status(404).json({ err })
-        }
-    } else {
-        res.status(404).end()
+router.delete('/:id', findBlog, async (req, res, next) => {
+    try {
+        await req.blog.destroy()
+        return res.status(200).end()
+    } catch (err) {
+        next(err)
     }
 })
 
-router.put('/:id', findBlog, async (req, res) => {
-    if (req.blog) {
+router.put('/:id', findBlog, async (req, res, next) => {
+    try {
         req.blog.likes = req.body.likes
         await req.blog.save()
         return res.status(200).end()
-    } else {
-        res.status(404).end()
+    } catch (err) {
+        next(err)
     }
 })
+
+const errorHandler = (err, _req, res, next) => {
+    console.error('errorHandler: ', err)
+    console.error('errorHandler errname:', err.name)
+    
+    switch (err.name) {
+        case 'TypeError': {
+            return res.status(400).send({ error: 'Can\'t find blog' })
+        }
+        case 'SequelizeValidationError': {
+            return res.status(404).json({ error: err.message })
+        }
+        default: next(err)
+    }
+}
+
+router.use(errorHandler)
 
 module.exports = router
