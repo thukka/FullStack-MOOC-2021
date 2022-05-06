3709826045e7f97d14295026f1ccaca891d385e3
Toni Hukka <berendin@gmail.com>
2022-05-06 09:00:55 +0300

    (13.18) added column 'year' to blogs via migration

--- part13/blog-backend/models/blog.js
+++ part13/blog-backend/models/blog.js
@@ -23,6 +23,13 @@ Blog.init({
         type: DataTypes.INTEGER,
         defaultValue: 0,
         allowNull: false
+    },
+    year: {
+        type: DataTypes.INTEGER,
+        validate: {
+            min: 1991,
+            max: new Date().getFullYear()
+        }
     }
 }, {
     sequelize,
