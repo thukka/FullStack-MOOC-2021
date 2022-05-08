b3be86f1ee68a71b97aeddd94247e8a3704d1e79
Toni Hukka <berendin@gmail.com>
2022-05-08 11:17:21 +0300

    (13.22) user can change their reading list read status

--- part13/blog-backend/controllers/reading_list.js
+++ part13/blog-backend/controllers/reading_list.js
@@ -1,5 +1,6 @@
 const router = require('express').Router()
 const { ReadingList } = require('../models/')
+const tokenExtractor = require('./utils/tokenExtractor')
 
 router.post('/', async (req, res) => {
     try {
@@ -8,6 +9,22 @@ router.post('/', async (req, res) => {
         res.status(200).json(entry)
     } catch (err) {
         res.status(400).json(err)
+    }
+})
+
+router.put('/:id', tokenExtractor, async (req, res) => {
+    try {
+        const findEntry = await ReadingList.findByPk(req.params.id)
+
+        if (req.decodedToken.id !== findEntry.userId) {
+            return res.status(400).json({ error: 'unauthorized' })
+        }
+
+        findEntry.read = req.body.read;
+        await findEntry.save()
+        res.status(200).json(findEntry)
+    } catch (err) {
+        res.status(401).json({ error: err })
     }
 })
 
